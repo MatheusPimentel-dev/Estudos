a2e7dc70ec5f268c1c9285ca8409a8f4559f6cc5
Matheus Pimentel <matheuspimentel.pimentel@gmail.com>
2023-10-28 11:21:14 -0300

    Aula 3

--- formacao-dart/dart-variaveis-listas/projeto_inicial/bin/projeto_inicial.dart
+++ formacao-dart/dart-variaveis-listas/projeto_inicial/bin/projeto_inicial.dart
@@ -4,21 +4,18 @@ void main() {
   int idade = 22;
   double altura = 1.86;
   double expone = 780e6;
-  bool geek = (idade < 20);
-
-  String nome = 'Matheus Pimentel';
+  bool geek = true;
+  const String nome = 'Matheus Pimentel';
   String apelido = 'Pimentel';
-
-  String frase = 'Eu sou $apelido'
-      'mas meu nome completo é: $nome. \n'
-      'Tenho $altura de altura e'
-      'tenho $idade anos de idade';
 
   List<String> listanomes = ['Ricarth', 'Natália', 'Alex', 'Ândriu', 'André'];
 
-  // print(idade);
-  // print(altura);
-  // print(expone);
-  // print(geek);
-  print(listanomes.length);
+  List<dynamic> matheus = [idade, altura, geek, nome, apelido];
+
+  String frase = 'Eu sou ${matheus[4]} '
+      'mas meu nome completo é: ${matheus[3]}. \n'
+      'Tenho ${matheus[1]} de altura e'
+      ' tenho ${matheus[0]} anos de idade';
+
+  print(frase);
 }
