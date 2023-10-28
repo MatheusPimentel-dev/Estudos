b3664a8dfb00485544e738573a7d802c864ebe14
Matheus Pimentel <matheuspimentel.pimentel@gmail.com>
2023-10-28 14:04:18 -0300

    Aula 5

--- formacao-dart/dart-variaveis-listas/projeto_inicial/bin/projeto_inicial.dart
+++ formacao-dart/dart-variaveis-listas/projeto_inicial/bin/projeto_inicial.dart
@@ -1,6 +1,8 @@
 import 'package:projeto_inicial/projeto_inicial.dart' as projeto_inicial;
 
 void main() {
+  /// Comentário de Documentação
+
   int idade = 22;
   double altura = 1.86;
   double expone = 780e6;
@@ -10,23 +12,29 @@ void main() {
   bool maiorDeIdade;
   int energia = 100;
 
+  //Verificando se o usuário é maior de idade
   if (idade >= 18) {
     maiorDeIdade = true;
   } else {
     maiorDeIdade = false;
   }
 
+  //Quantas linhas podemos imprimir
   for (int i = 0; i < 5; i++) {
     print('Total de linhas: $i');
   }
 
+  //Quantas repetições pode ser feita
   while (energia > 0) {
     print('Mais uma repetição');
     energia = energia - 10;
   }
 
-  List<String> listanomes = ['Ricarth', 'Natália', 'Alex', 'Ândriu', 'André'];
+  //Lista de String
+  //List<String> listanomes = ['Ricarth', 'Natália', 'Alex', 'Ândriu', 'André'];
+  //TODO: Fazer outras repetições
 
+  //Lista dos usuários
   List<dynamic> matheus = [idade, altura, geek, nome, apelido];
 
   String frase = 'Eu sou ${matheus[4]} '
